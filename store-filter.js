fe6c3aad12885d4b494c44bee26d8efc97fa0491
senchuri <63138330+senchuri@users.noreply.github.com>
2025-05-26 23:39:29 +0545

    Update store-filter.js

--- store-filter.js
+++ store-filter.js
@@ -417,7 +417,7 @@ function loadProducts(){
   list.innerHTML = filtered.length ? 
     filtered.map(p => `
       <div style="border:1px solid#ccc;padding:10px;margin:10px 0">
-      <img src=${p.ImageURL} style={width:100%; height:250px; object-fit:cover} />
+        <img src="${p.ImageURL}" style="width:100%; height:250px; object-fit:cover;" />
         <h3>${p.ProductName}</h3>
         <p>${p.Category}</p>
         <p>Vendor: ${p.Vendor}</p>
